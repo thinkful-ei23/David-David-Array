2eb988101e384146f8b9f0ecda55c515cefbaefc
David Folks <aldwyn.wot@gmail.com>
2018-07-17 10:49:01 -0600

    added part 2 to functionsAsArguments.js

--- functionsAsArguments.js
+++ functionsAsArguments.js
@@ -1,3 +1,4 @@
+/*
 function repeat(fn, n) {
     for (let i = 0; i < n; i++) {
         fn();
@@ -14,3 +15,27 @@ function goodbye() {
 
 repeat(hello, 5);
 repeat(goodbye, 5);
+*/
+'use strict';
+// DO NOT EDIT BETWEEN THESE LINES ----->
+// Return only names that begin with 'R'
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+
+const filteredNames = filter(myNames, function(name) {
+  // This is a "predicate function" - it's a function that only returns a boolean
+  return name[0] === 'R';
+});
+
+console.log(filteredNames); // => ['Rich', 'Ray']
+// <---- DO NOT EDIT BETWEEN THESE LINES
+
+// TASK: DEFINE YOUR FILTER FUNCTION BELOW:
+function filter(arr, fn) {
+  let newArray = [];
+  for (let i = 0;i < arr.length; i++) {
+    if (fn(arr[i]) === true) {
+      newArray.push(arr[i]);
+    }
+  }
+  return newArray;
+}
