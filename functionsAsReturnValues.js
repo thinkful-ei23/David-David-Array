e9d79afc167d5f70c7ef065b6530743f364de492
David Graves <davidgraves831@gmail.com>
2018-07-17 13:38:29 -0500

    completed functionAsReturnValues

---
+++ functionsAsReturnValues.js
@@ -0,0 +1,25 @@
+function hazardWarningCreator (typeOfWarning){
+    let warningCounter = 0;
+
+    return function(location){
+        warningCounter++;
+
+        console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`);
+        console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`);
+        
+    }
+    
+}
+
+const rocksWarning = hazardWarningCreator('Rocks on the Road');
+const branchesWarning = hazardWarningCreator('Branches on the Road');
+const snowWarning = hazardWarningCreator('Snow on the Road');
+
+rocksWarning('Main St and Pacific Ave');
+rocksWarning('Centinela Ave and Olympic Blvd');
+branchesWarning('Denver Colorado');
+branchesWarning('Monterey California');
+snowWarning('Santa Barbara');
+snowWarning('Tahoe California');
+
+console.log(rocksWarning);
